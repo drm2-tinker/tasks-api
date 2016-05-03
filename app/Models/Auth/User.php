79818819c7f5b7c4efe69456046833757ce130fd
drmyersii <davidrmyersii@gmail.com>
2016-05-03 17:43:59 -0400

    Implementing Auth contracts and traits into the customized Sentinel User model.

--- app/Models/Auth/User.php
+++ app/Models/Auth/User.php
@@ -3,11 +3,15 @@
 namespace App\Models\Auth;
 
 use Cartalyst\Sentinel\Users\EloquentUser;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
-class User extends EloquentUser
+class User extends EloquentUser implements AuthenticatableContract, AuthorizableContract
 {
     use \App\Models\Traits\UuidKey;
     use \Illuminate\Database\Eloquent\SoftDeletes;
+    use \Illuminate\Auth\Authenticatable;
+    use \Illuminate\Foundation\Auth\Access\Authorizable;
 
     // disable auto-incrementing key generation for models
     public $incrementing = false;
